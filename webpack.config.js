4064cfaab5dad16bd052059a4c1a2aaafa830012
Bryan Lackey <lackeyjb@users.noreply.github.com>
2015-09-13 23:40:41 -0400

    added hot reloading with express & webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var Webpack = require('webpack');
+var path = require('path');
+var appPath = path.resolve(__dirname, 'app');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'app', 'main.js');
+
+var config = {
+  context: __dirname,
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
+    mainPath],
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: [nodeModulesPath]
+    }]
+  },
+  plugins: [new Webpack.HotModuleReplacementPlugin()]
+};
+
+module.exports = config;
